d3a5ffef37ddfbf06f4b5b23cf0ebf08bee20801
Shivam Wadhwa <shivamwadhwa7877@gmail.com>
2026-05-29 11:58:46 +0530

    Build: Fetch data from API and display on store page.

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -1,49 +1,36 @@
+import { useEffect, useState } from "react";
 import styles from "./Home.module.css";
 import Quantity from "./Quantity";
 import mainStyles from "./Shop.module.css";
 
 function Shop(){
+
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+         fetch('https://fakestoreapi.com/products')
+            .then(response => response.json())
+            .then((data) => {
+                setProducts(data);
+                setLoading(false);
+            });
+
+    },[]);
+
+
     return(
         <>
         <div className= {styles.primaryContainer}>
             {/* <h1>This is the shop page</h1> */}
             <div className = {mainStyles.cardsParentDiv}>
-                <div className= {mainStyles.cardsDiv}>
-                    <span className= {mainStyles.productTitle}>Oxfords</span>
-                    <Quantity />
-                </div>
-                <div className= {mainStyles.cardsDiv}>
-                    <span className= {mainStyles.productTitle}>Derbies</span>
-                    <Quantity />
-                </div>
-                <div className= {mainStyles.cardsDiv}>
-                    <span className= {mainStyles.productTitle}>Loafers</span>
-                    <Quantity />
-                </div>
-                <div className= {mainStyles.cardsDiv}>
-                    <span className= {mainStyles.productTitle}>Brogues</span>
-                    <Quantity />
-                </div>
-                <div className= {mainStyles.cardsDiv}>
-                    <span className= {mainStyles.productTitle}>Chelsea Boots</span>
-                    <Quantity />
-                </div>
-                <div className= {mainStyles.cardsDiv}>
-                    <span className= {mainStyles.productTitle}>Sneakers</span>
-                    <Quantity />
-                </div>
-                <div className= {mainStyles.cardsDiv}>
-                    <span className= {mainStyles.productTitle}>Monk Strap Shoes</span>
-                    <Quantity />
-                </div>
-                <div className= {mainStyles.cardsDiv}>
-                    <span className= {mainStyles.productTitle}>Boat Shoes</span>
-                    <Quantity />
-                </div>
-                <div className= {mainStyles.cardsDiv}>
-                    <span className= {mainStyles.productTitle}>Chukka Shoes</span>
-                    <Quantity />
-                </div>
+                {products.map(product => {
+                    return(    
+                    <div className= {mainStyles.cardsDiv} key={product.id}>
+                        <span className= {mainStyles.productTitle}>{product.title}</span> 
+                        <img src={product.image} alt={product.title} className={mainStyles.productImage}/>                   
+                        <Quantity />
+                    </div>
+                )})}
             </div>
         </div>
         </>
