20aeb6a1e195f617d469817979e2b4a86e54948f
Shivam Wadhwa <shivamwadhwa7877@gmail.com>
2026-05-29 13:31:03 +0530

    Built: Quantity Component Completed.

--- src/Components/Quantity.jsx
+++ src/Components/Quantity.jsx
@@ -2,26 +2,31 @@ import { useState } from "react";
 import styles from "./Shop.module.css";
 
 function Quantity(){
-    const [quantity, setQuantity] = useState();
+    const [quantity, setQuantity] = useState(0);
 
     function increaseHandle(){
-        setQuantity(quantity + 1);
+        setQuantity(prev => prev + 1);
     }
     function decreaseHandle(){
-        if (quantity >0){
-            setQuantity(quantity - 1);
+        if (quantity > 0){
+            setQuantity(prev => prev - 1);
         }
     }
-    return (
+
+    function handleChange(e){
+        setQuantity(Number(e.target.value));
+    }
+    return (    
         <>
             <div className = {styles.quantityDiv}>
                 <label htmlFor="quantity">Quantity: </label>
-                <input type="number" name="quantity" className={styles.input}/>
+                <input type="number" name="quantity" className={styles.input} value={quantity} onChange={handleChange}/>
                 <div className= {styles.plusMinusButtons}>
                     <img src="/src/assets/plus.png" alt="Plus Image" className= {styles.plusMinusItemBtn} onClick={increaseHandle}/>
                     <img src="/src/assets/minus.png" alt="Minus Image" className= {styles.plusMinusItemBtn} onClick={decreaseHandle}/>
                 </div>
                 <button type="submit" className= {styles.button}>Add to Cart</button>
+                <p>{quantity}</p>
             </div>
         </>
     )
--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -21,13 +21,15 @@ function Shop(){
     return(
         <>
         <div className= {styles.primaryContainer}>
+            {loading && <h1>Loading...</h1>}
             {/* <h1>This is the shop page</h1> */}
             <div className = {mainStyles.cardsParentDiv}>
                 {products.map(product => {
                     return(    
                     <div className= {mainStyles.cardsDiv} key={product.id}>
                         <span className= {mainStyles.productTitle}>{product.title}</span> 
-                        <img src={product.image} alt={product.title} className={mainStyles.productImage}/>                   
+                        <img src={product.image} alt={product.title} className={mainStyles.productImage}/>              
+                        <h2>${product.price}</h2>     
                         <Quantity />
                     </div>
                 )})}
